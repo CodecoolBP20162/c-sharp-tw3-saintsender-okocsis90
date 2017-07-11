63e41b488738836888ae691dbec5290c517f8c17
okocsis90 <o.kocsis90@gmail.com>
2017-07-11 14:30:49 +0200

    base structure added

---
+++ SaintSender/SaintSender/EmailClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaintSender
+{
+    public partial class EmailClient : Form
+    {
+        public EmailClient()
+        {
+            InitializeComponent();
+        }
+    }
+}
